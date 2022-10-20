158eac33a2f08f19394445d035d8d7a03db2040e
HIDRI A <agthidri24@yahoo.com>
2022-10-19 21:33:54 -0500

    this commit will add first

---
+++ eRegistrarLab10/src/main/java/com/example/eregistrarlab10/ERegistrarLab10Application.java
@@ -0,0 +1,13 @@
+package com.example.eregistrarlab10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERegistrarLab10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERegistrarLab10Application.class, args);
+	}
+
+}
---
+++ eRegistrarLab10/src/test/java/com/example/eregistrarlab10/ERegistrarLab10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eregistrarlab10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERegistrarLab10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
